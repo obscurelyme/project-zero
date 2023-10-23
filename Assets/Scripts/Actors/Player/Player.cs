7fb397bfd9d18871794d38f7df31a71e9dc708c1
Mackenzie Greco <nico.frozencoffee@gmail.com>
2023-10-23 12:03:20 -0700

    player clamped movement and rotation

--- Assets/Scripts/Actors/Player/Player.cs
+++ Assets/Scripts/Actors/Player/Player.cs
@@ -11,10 +11,19 @@ public class Player : MonoBehaviour
   // Transform playerTransform;
   UnityEngine.Vector2 movement;
   UnityEngine.Vector3 currentPos;
-  UnityEngine.Vector3 newPos;
+  UnityEngine.Quaternion currentRot;
+  UnityEngine.Quaternion leftRoation;
+  UnityEngine.Quaternion rightRoation;
+  UnityEngine.Quaternion forwardRoation;
 
   [SerializeField]
-  int movementSpeed = 10;
+  float rotateSpeed = 2.5f;
+  [SerializeField]
+  float movementSpeed = 7.0f;
+  [SerializeField]
+  float clampLeft = -8.0f;
+  [SerializeField]
+  float clampRight = 8.0f;
 
   void Awake()
   {
@@ -24,22 +33,34 @@ public class Player : MonoBehaviour
 
   void Start()
   {
-    // playerTransform = GetComponent<Transform>();
+    leftRoation.eulerAngles = new UnityEngine.Vector3(0f, 0f, 10f);
+    rightRoation.eulerAngles = new UnityEngine.Vector3(0f, 0f, -10f);
+    forwardRoation.eulerAngles = new UnityEngine.Vector3(0f, 0f, 0f);
   }
 
   void Update()
   {
     currentPos = transform.position;
+    currentRot = transform.rotation;
     movement = inputActions.Player.Move.ReadValue<UnityEngine.Vector2>();
     if (movement.x == -1)
     {
       // NOTE: go left
-      currentPos.x -= movementSpeed * Time.deltaTime;
+      currentPos.x = UnityEngine.Mathf.Clamp(currentPos.x - movementSpeed * Time.deltaTime, clampLeft, clampRight);
+      // NOTE: rotate to the left
+      transform.rotation = UnityEngine.Quaternion.RotateTowards(transform.rotation, leftRoation, rotateSpeed * Time.deltaTime);
     }
     if (movement.x == 1)
     {
       // NOTE: go right
-      currentPos.x += movementSpeed * Time.deltaTime;
+      currentPos.x = UnityEngine.Mathf.Clamp(currentPos.x + movementSpeed * Time.deltaTime, clampLeft, clampRight);
+      // NOTE: rotate to the right
+      transform.rotation = UnityEngine.Quaternion.RotateTowards(transform.rotation, rightRoation, rotateSpeed * Time.deltaTime);
+    }
+    if (movement.x == 0)
+    {
+      // NOTE: rotate to the center again
+      transform.rotation = UnityEngine.Quaternion.RotateTowards(transform.rotation, forwardRoation, rotateSpeed * Time.deltaTime);
     }
 
     transform.position = currentPos;
